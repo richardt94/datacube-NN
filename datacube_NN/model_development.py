bdd18c7c4a88af2dd3b838b245deb71c1ee05db8
Richard Taylor <29562583+richardt94@users.noreply.github.com>
2020-01-24 16:48:06 +1100

    add functionality to append resampled slices of geoTIFF files to the datacube product as auxiliary covariates

--- datacube_NN/model_development.py
+++ datacube_NN/model_development.py
@@ -2,6 +2,7 @@ from datacube.utils.geometry import CRS
 import numpy as np
 import tifffile
 import pandas as pd
+import xarray as xr
 import os
 from tensorflow.keras.utils import Sequence
 from tensorflow.keras.preprocessing.image import apply_affine_transform
@@ -11,7 +12,9 @@ class DatacubeHarvester:
     Harvests labelled examples from geospatial raster data stored in a 
     datacube into a format easily ingested by Keras models.
     """
-    def __init__(self, dc, product, pixel_buffer = (10,10), **query):
+    def __init__(self, dc, product, pixel_buffer = (10,10),
+                 geotiffs=[], dask_chunks={'x':1000,'y':1000},
+                 interpolation = None, **query):
         """
         Initialise a DatacubeHarvester, which contains attributes relevant
         to *all* samples: the datacube handle itself, product to be loaded
@@ -21,6 +24,13 @@ class DatacubeHarvester:
         for products with a time dimension will be used - this is to ensure
         consistency between samples where different locations may have
         different numbers of observations within the same timeframe.
+        
+        geotiffs is an array of geotiff filenames. These should have the same
+        CRS as the datacube product being loaded. They are assumed to be v big
+        and dask chunking is used to open them by default to deal with this.
+        
+        Geotiffs may be interpolated (by default nearest-neighbour, no interp)
+        when resampling to the same grid as the datacube product samples.
         """
         self.dc = dc
         self.prod = product
@@ -43,22 +53,44 @@ class DatacubeHarvester:
             res_dict['y'] = res_dict.pop('lat')
         self.resolutions = res_dict
         
-        self.spatial_buffer = {}
+        spatial_buffer = {}
         #y-first indexing for consistency with DEA datacube (maybe all ODCs)
-        self.spatial_buffer['y'] = pixel_buffer[0]*res_dict['y']
-        self.spatial_buffer['x'] = pixel_buffer[1]*res_dict['x']
+        spatial_buffer['y'] = pixel_buffer[0]*res_dict['y']
+        spatial_buffer['x'] = pixel_buffer[1]*res_dict['x']
         
         #precompute the offsets from the sample site for each pixel in the final image
-        self.reindex_masks = { dim : np.arange(-self.spatial_buffer[dim],
-                                               self.spatial_buffer[dim]+res_dict[dim],
+        self.reindex_masks = { dim : np.arange(-spatial_buffer[dim],
+                                               spatial_buffer[dim]+res_dict[dim],
                                                res_dict[dim])
-                              for dim in self.spatial_buffer }
+                              for dim in spatial_buffer }
         #include the tolerance in the dc.load to avoid missing values on
         #edge of the spatial buffer
         self.tol =   {
                 'x': abs(self.resolutions['x'])/2,
                 'y': abs(self.resolutions['y'])/2
                 }
+        
+        self.qbuffer = {
+            'x': abs(spatial_buffer['x'])+self.tol['x'],
+            'y': abs(spatial_buffer['y'])+self.tol['y']
+        }
+        
+        self.extra_data = False
+        if len(geotiffs):
+            self.extra_data = True
+            self.tiff_list = []
+            self.indexing_order = [] #to assist in slicing the xarray dataset correctly
+            self.tiff_res=[]
+            for fname in geotiffs:
+                tiffds = xr.open_rasterio(fname, chunks=dask_chunks)
+                #are the coordinates in descending order? -1 for yes 1 for no
+                self.indexing_order.append({'x': 1-2*(tiffds.x[-1]<tiffds.x[0]),
+                                            'y': 1-2*(tiffds.y[-1]<tiffds.y[0])})
+                self.tiff_list.append(tiffds)
+                self.tiff_res.append({'x': abs(tiffds.x[1] - tiffds.x[0]),
+                                     'y': abs(tiffds.y[1] - tiffds.y[0])})
+                
+            
         
     def process(self,savedir, xyw_df, x_col, y_col, w_col = None, nan_value = 0):
         """
@@ -72,11 +104,12 @@ class DatacubeHarvester:
             point = point.to_crs(self.native_crs)
             (sitex,sitey) = point.coords[0]
             
+            
             covars = self.dc.load(product = self.prod,
-                             x = (sitex-abs(self.spatial_buffer['x'])-self.tol['x'],
-                                  sitex+abs(self.spatial_buffer['x'])+self.tol['x']),
-                             y = (sitey-abs(self.spatial_buffer['y'])-self.tol['y'],
-                                  sitey+abs(self.spatial_buffer['y'])+self.tol['y']),
+                             x = (sitex-self.qbuffer['x'],
+                                  sitex+self.qbuffer['x']),
+                             y = (sitey-self.qbuffer['y'],
+                                  sitey+self.qbuffer['y']),
                              crs = self.native_crs,
                              **self.query)
             
@@ -90,13 +123,36 @@ class DatacubeHarvester:
                                     method = 'nearest',
                                     tolerance = self.tol['y'])
             
-            covars = covars.fillna(nan_value)
             
             if 'time' in covars.dims:
                 covars = covars.isel(time=0)
+                covars = covars.reset_coords(drop=True)
             
             #needs to be a dataarray to turn to numpy
             covars = covars.to_array()
+            
+            if self.extra_data:
+                for res,iord,tiffds in zip(self.tiff_res, self.indexing_order, self.tiff_list):
+                    #select a window from the tiff and load to memory
+                    tiffw = tiffds.sel(
+                        x=slice(sitex-iord['x']*self.qbuffer['x'],
+                                sitex+iord['x']*self.qbuffer['x']),
+                        y=slice(sitey-iord['y']*self.qbuffer['y'],
+                                sitey+iord['y']*self.qbuffer['y'])
+                    ).compute()
+                    #reindex to be compatible with covars, tolerance to one pixel
+                    #of the tiff
+                    
+                    tiffw = tiffw.reindex(x = self.reindex_masks['x']+sitex,
+                                        method = 'nearest',
+                                        tolerance = res['x'])
+                    tiffw = tiffw.reindex(y = self.reindex_masks['y']+sitey,
+                                        method = 'nearest',
+                                        tolerance = res['y'])
+                    tiffw = tiffw.rename({'band':'variable'})
+                    
+                    covars = xr.concat([covars, tiffw], dim='variable')
+            covars = covars.fillna(nan_value)                    
             
             return covars.data
 
@@ -156,7 +212,7 @@ class MultiTiffGenerator(Sequence):
             self.gen_df = gen_df
             
         self.fnames = gen_df[x_col].to_numpy()
-        self.yvals = pd.to_numeric(gen_df[y_col]).to_numpy()
+        self.yvals = gen_df[y_col]
         
         if w_col is not None:
             self.weights = gen_df[w_col]
